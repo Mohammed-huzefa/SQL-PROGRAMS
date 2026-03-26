f0bf07ee0f28c5b80445cdbcca6817b69b7057ae
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-03-26 12:32:34 +0530

    Update prg4.sql

--- prg4.sql
+++ prg4.sql
@@ -1,34 +1,23 @@
-create database company;
-use company;
+Create Table: 
+> create table Customers (id number, name varchar(10), age number, sal number, address varchar(50));
 
-create table Customers (
-id int,
-name varchar(10),
-age int,
-salary int,
-address varchar(50)
-);
+Insert records :
+>insert into Customers values(10,'abhi',25 ,10000,”Bangalore”); 
+>insert into Customers values(20,'rohith',30,9000, ”Delhi”); 
+>insert into Customers values(30,'david',28,9000, ”Pune”);
 
-insert into Customers values(10,'abhi',25,10000,'Bangalore');
-insert into Customers values(20,'rohith',30,9000,'Delhi');
-insert into Customers values(30,'david',28,9000,'Pune');
-
-delimiter //
-
-create trigger sal_diff_update
-before update on Customers
-for each row
-begin
-    declare sal_diff int;
-    set sal_diff = new.salary - old.salary;
-
-    select 'Previous salary: ', old.salary;
-    select 'Current salary: ', new.salary;
-    select 'Salary difference: ', sal_diff;
-end //
-
-delimiter ;
-
-update Customers set salary = 12000 where id = 10;
-
-select * from Customers;
+Creating trigger:
+>set serveroutput on; 
+1 create or replace TRIGGER sal_diff 
+2 Before Delete or INSERT OR UPDATE on Customer 
+3 for each row 
+4 when(new.id>0) 
+5 Declare 
+6 	sal_diff number; 
+7 BEGIN 
+8 	sal_diff := :NEW.sal - :OLD.sal; 
+9	 dbms_output.put_line('Previous salary: ' || : OLD.sal); 
+10 	 dbms_output.put_line('Current salary ' || : NEW.sal); 
+11	 dbms_output.put_line ('salary difference: ' || sal_diff); 
+12 END; 
+13 / 
