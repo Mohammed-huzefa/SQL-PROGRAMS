e1820c09e8bb94bc166445e53375d8e94576bdf0
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-05-19 21:52:01 +0530

    Create mongodb.js

---
+++ mongodb.js
@@ -0,0 +1,74 @@
+mongosh
+
+
+
+use mydb
+
+
+
+db.createCollection("users")
+
+
+
+db.users.insertOne({
+  name: "Alice",
+  age: 25,
+  city: "Bangalore"
+})
+
+
+
+db.users.insertMany([
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 28 }
+])
+
+
+
+db.users.find()
+
+
+
+db.users.find().pretty()
+
+
+
+db.users.find({ name: "Alice" })
+
+
+
+db.users.updateOne(
+  { name: "Alice" },
+  { $set: { age: 26 } }
+)
+
+
+
+db.users.updateMany(
+  {},
+  { $set: { active: true } }
+)
+
+
+
+db.users.deleteOne({ name: "Bob" })
+
+
+
+db.users.deleteMany({ age: { $lt: 29 } })
+
+
+
+show dbs
+
+
+
+show collections
+
+
+
+db.users.drop()
+
+
+
+db.dropDatabase()
